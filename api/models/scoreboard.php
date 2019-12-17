a5a913630afd0e32586d076cffb02e17486e29c3
Sam Bernal <sambernaldamasco@gmail.com>
2019-12-17 12:52:14 -0500

    maybe this will work?

--- api/models/scoreboard.php
+++ api/models/scoreboard.php
@@ -1,5 +1,22 @@
 <?php
-$dbconn = pg_connect("host=localhost dbname=trivia");
+$dbconn = null;
+if(getenv('DATABASE_URL')){
+    $connectionConfig = parse_url(getenv('DATABASE_URL'));
+    $host = $connectionConfig['host'];
+    $user = $connectionConfig['user'];
+    $password = $connectionConfig['pass'];
+    $port = $connectionConfig['port'];
+    $dbname = trim($connectionConfig['path'],'/');
+    $dbconn = pg_connect(
+        "host=".$host." ".
+        "user=".$user." ".
+        "password=".$password." ".
+        "port=".$port." ".
+        "dbname=".$dbname
+    );
+} else {
+    $dbconn = pg_connect("host=localhost dbname=trivia");
+}
 
 
 class Score {
